6d09c8a17883146fb334bbed6221ce43df784a08
William Sampaio <56176702+WilliamSampaio@users.noreply.github.com>
2024-06-05 14:13:15 -0400

    translation base

---
+++ translation.js
@@ -0,0 +1,32 @@
+const translation = {
+    "en-US": {
+        "txt_hallo": "Hello World",
+        "txt_title": "Bible Online",
+        "txt_navbar_about": "About",
+        "txt_navbar_language": "Language"
+    },
+    "pt-BR": {
+        "txt_hallo": "Olá Mundo",
+        "txt_title": "Bíblia Online",
+        "txt_navbar_about": "Sobre",
+        "txt_navbar_language": "Idioma"
+    }
+}
+
+const default_app_lang = "en-US";
+
+const url_params = new URLSearchParams(window.location.search);
+
+if (sessionStorage.getItem("app_lang") === null) {
+    sessionStorage.setItem("app_lang", url_params.get("lang") || default_app_lang);
+} else if (url_params.get("lang") !== null) {
+    sessionStorage.setItem("app_lang", url_params.get("lang"));
+}
+
+const translate = function () {
+    for (const [key, value] of Object.entries(translation[sessionStorage.getItem("app_lang")])) {
+        document.querySelectorAll(`.${key}`).forEach(el => {
+            el.textContent = value;
+        })
+    }
+}
